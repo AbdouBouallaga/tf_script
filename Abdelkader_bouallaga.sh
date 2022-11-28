88cc9a8a868d77566d86364836a233281dcbb76e
Abdelkader Bouallaga <babdelka@student.1337.ma>
2022-11-28 21:07:53 +0100

    update

--- Abdelkader_bouallaga.sh
+++ Abdelkader_bouallaga.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-project_id=$(gcloud config get-value project)
-git clone https://github.com/AbdouBouallaga/tf_script.git tfinfra
-cd tfinfra
+#project_id=$(gcloud config get-value project)
+if [ -d "./tfinfra" ]; then
+	cd tfinfra
+	git pull
+else
+	git clone https://github.com/AbdouBouallaga/tf_script.git tfinfra
+	cd tfinfra
+fi
 terraform init
 terraform fmt -recursive
 terraform plan && terraform apply -auto-approve
